7c113e10b6e668ce33215389489e4f743d9a3d9f
seoah <24seoah@sookmyung.ac.kr>
2025-11-25 18:59:47 +0900

    feat: intro에 js 추가

--- js/8_tomato_intro.js
+++ js/8_tomato_intro.js
@@ -14,3 +14,38 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+/***********************************************
+ * TRACK CARD 애니메이션
+ ***********************************************/
+const trackCards = document.querySelectorAll('.track-card');
+
+const trackObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.3 });
+
+trackCards.forEach(card => trackObserver.observe(card));
+
+
+/***********************************************
+ * TRACK 제목/설명 애니메이션
+ ***********************************************/
+const introTrack = document.querySelector('.intro-track');
+
+const trackTitleObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      introTrack.classList.add('show');
+    }
+  });
+}, { threshold: 0.3 });
+
+trackTitleObserver.observe(introTrack);
+
+
+
+
